2a4ae3143a580f6a5d34dcdf565f4c247021c7fd
Travis Alstrand <travisalstrand@gmail.com>
2023-07-16 16:50:56 -0700

    copied files from udemy to get passed error. installed navigation

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
-import { ThemeProvider } from "styled-components";
 
 import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
